7e7dce9c86109f5a6de513ab13bc80af414a4a51
Shrishail Gharat <52442744+shrigharat@users.noreply.github.com>
2023-01-08 15:59:24 +0530

    Upgraded to public show pages

--- pages/show/[showId].js
+++ pages/show/[showId].js
@@ -2,7 +2,7 @@ import Head from "next/head";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import Header from "../../components/header/header";
-import { getUserIdFromToken } from "../../lib/utils";
+import { magicClient } from "../../lib/magic-client";
 import { getVideoDetailsById, processVideoDetails } from "../../lib/videos";
 import styles from "../../styles/Show.module.css";
 
@@ -20,17 +20,14 @@ export async function getStaticPaths() {
     }
 }
 
-export async function getStaticProps({ params, req }) {
-    const token = req?.cookies?.token;
-    const userId = getUserIdFromToken(token);
-
+export async function getStaticProps({ params }) {
     const { showId } = params;
     const showDetails = await getVideoDetailsById(showId);
     const processedDetails = processVideoDetails(showDetails);
 
     return {
         props: {
-            showDetails: processedDetails
+            showDetails: processedDetails || ""
         },
         revalidate: 10
     }
@@ -38,22 +35,20 @@ export async function getStaticProps({ params, req }) {
 
 const ShowPage = ({ showDetails }) => {
     const router = useRouter();
+    const [isLoggedIn, setLoggedIn] = useState(false);
     const { showId } = router.query;
     const [saved, setSaved] = useState(false);
 
-    const handleSaveToWatchlist = async () => {
-        const toSave = !saved;
-        const response = await fetch('/api/stats', {
-            method: "POST",
-            body: JSON.stringify({ videoId: showId, watched: true, saved: toSave }),
-            headers: {
-                "Content-Type": "application/json"
-            }
-        });
-        const data = await response.json();
-        setSaved(data.stats.saved);
-    }
+    //check logged in status 
+    useEffect(() => {
+        const checkUserState = async () => {
+            const loggedIn = await magicClient.user.isLoggedIn();
+            setLoggedIn(loggedIn);
+        };
+        checkUserState();
+    }, []);
 
+    //handler for stats api
     useEffect(() => {
         const createStats = async () => {
             const showStatsResponse = await fetch(`/api/stats?videoId=${showId}`, {
@@ -77,38 +72,85 @@ const ShowPage = ({ showDetails }) => {
                 setSaved(data.stats.saved);
             }
         }
-        createStats();
-    }, []);
 
-    const { title, description, publishTime, channelTitle, viewCount } = showDetails;
+        if (isLoggedIn) {
+            createStats();
+        }
+    }, [isLoggedIn]);
+
+    const handleSaveToWatchlist = async () => {
+        if (!isLoggedIn) {
+            router.push('/login');
+            return;
+        }
+        const toSave = !saved;
+        const response = await fetch('/api/stats', {
+            method: "POST",
+            body: JSON.stringify({ videoId: showId, watched: true, saved: toSave }),
+            headers: {
+                "Content-Type": "application/json"
+            }
+        });
+        const data = await response.json();
+        setSaved(data.stats.saved);
+    }
+
+    const { title = "Netflix", description = "", publishTime, channelTitle, viewCount } = showDetails;
 
     return <div className={styles.showPage}>
         <Header />
         <Head>
             <title>{title}</title>
-            <meta name="description" content={description.substr(0,16) + "..."} />
+            <meta name="title" content={title} />
+            <meta name="description" content={description.substr(0, 16) + "..."} />
+
+            <meta property="og:type" content="website" />
+            <meta property="og:title" content={title} />
+            <meta property="og:description" content={description.substr(0, 16) + "..."} />
+            <meta property="og:image" content={`https://i.ytimg.com/vi/${showId}/mqdefault.jpg`} />
+
+            <meta property="twitter:card" content={title} />
+            <meta property="twitter:title" content={title} />
+            <meta property="twitter:description" content={description.substr(0, 16) + "..."} />
+            <meta property="twitter:image" content={`https://i.ytimg.com/vi/${showId}/mqdefault.jpg`} />
         </Head>
+
         <div className={styles.showPageWrapper}>
-            <iframe id="ytplayer"
-                type="text/html" width="100%" height="480"
-                src={`https://www.youtube.com/embed/${showId}?autoplay=1&origin=http://example.com&controls=0&modestbranding=1`}
-                frameBorder="0">
-            </iframe>
-            <div className={styles.showDetails}>
-                <div className={styles.col1}>
-                    <h1>{title}</h1>
-                    <span>{publishTime}</span>
-                    <p>{description}</p>
-                </div>
-                <div className={styles.col2}>
-                    <div>
-                        <span>By {channelTitle} <i className='bx bxs-check-circle' style={{ color: '#dc2626' }}></i></span>
-                        <br />
-                        <span>{viewCount} views</span>
+            {
+                showDetails ? <>
+                    <iframe id="ytplayer"
+                        type="text/html" width="100%" height="480"
+                        src={`https://www.youtube.com/embed/${showId}?autoplay=1&origin=http://example.com&controls=0&modestbranding=1`}
+                        frameBorder="0">
+                    </iframe>
+                    <div className={styles.showDetails}>
+                        <div className={styles.col1}>
+                            <h1>{title}</h1>
+                            <span>{publishTime}</span>
+                            <p>{description}</p>
+                        </div>
+                        <div className={styles.col2}>
+                            <div>
+                                <span>By {channelTitle} <i className='bx bxs-check-circle' style={{ color: '#dc2626' }}></i></span>
+                                <br />
+                                <span>{viewCount} views</span>
+                            </div>
+                            <button
+                                style={{
+                                    backgroundColor: saved ? "var(--white20)" : "",
+                                    color: saved ? "var(--black10)" : "var(--white30"
+                                }}
+                                onClick={handleSaveToWatchlist}
+                            >
+                                {!isLoggedIn ? "Login to save" : saved ? (<span>Saved to watchlist<i className='bx bx-check'></i></span>) : "Save to watchlist"}
+                            </button>
+                        </div>
                     </div>
-                    <button style={{ backgroundColor: saved ? "var(--white20)" : "", color: saved ? "var(--black10)" : "var(--white30" }} onClick={handleSaveToWatchlist}>{saved ? (<span>Saved to watchlist<i className='bx bx-check'></i></span>) : "Save to watchlist"}</button>
+                </> : <div className={styles.notFound}>
+                    <h1>Could not find the show!</h1>
+                    <p>Please try again later</p>
                 </div>
-            </div>
+            }
         </div>
     </div>;
 }
