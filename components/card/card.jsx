476b686d9ecffe859dc607eccf32b534193ab6b5
Shrishail Gharat <52442744+shrigharat@users.noreply.github.com>
2023-08-28 13:14:33 +0530

    Update card.jsx
    added image retry logic

--- components/card/card.jsx
+++ components/card/card.jsx
@@ -8,13 +8,29 @@ const Card = ({ size, imgURL, title }) => {
     large: styles.lgCard,
   };
 
+  const [imageHref, setImageHref] = useState(imgURL);
+  const [imageTries, setImageTries] = useState(0);
+  const fallbackImgurl = "/static/images/fallback_image.webp";
+
+  const onError = () => {
+    let tries = imageTries;
+    tries += 1;
+    setImageTries(prev => prev+1);
+
+    if(tries == 1) {
+      setImageHref(imgURL.replace('maxresdefault', 'hqdefault'));
+    } else if(tries == 2) {
+      setImageHref(fallbackImgurl);
+    }
+  }
+
   return (
     <div className={`${configMap[size]} ${styles.card}`}>
       {
         title && <div className={styles.overlay}></div>
       }
       <div className={styles.imgWrapper}>
-        <Image src={imgURL} layout="fill" />
+        <Image src={imageHref} layout="fill" onError={onError} />
       </div>
       <div className={styles.content}>
         <span>{title}</span>
